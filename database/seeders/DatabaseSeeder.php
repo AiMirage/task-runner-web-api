2f69e61eadf1449d8253372bbb798a4b2eb7bea7
Ali Ibrahim <aliibra395@gmail.com>
2022-04-26 20:19:01 +0200

    Seeded user instance for testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +17,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => "admin",
+            'email' => "admin@admin.com",
+            'email_verified_at' => now(),
+            'password' => Hash::make("123456"), // password
+            'remember_token' => null,
+        ]);
     }
 }
