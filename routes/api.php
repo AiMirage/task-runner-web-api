8be7b2f3ba8c2353b18dd70b8f63ffbb4f65e3c5
Ali Ibrahim <aliibra395@gmail.com>
2022-04-26 20:17:38 +0200

    Registered login route / auth middleware

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function () {
+
+    /**
+     * Authentication
+     */
+    Route::post('login', [\App\Http\Controllers\Auth\UserController::class, 'login']);
+
+
+
 });
