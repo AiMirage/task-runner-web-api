a2ba1436445b14dd0a5b51a7cd634f2643b3af08
Ali Ibrahim <aliibra395@gmail.com>
2022-04-27 00:21:53 +0200

    Implemented user repo

--- app/Repositories/UsersRepo.php
+++ app/Repositories/UsersRepo.php
@@ -2,7 +2,20 @@
 
 namespace App\Repositories;
 
+use App\Models\User;
+
 class UsersRepo extends BaseRepo
 {
 
+    protected array $fieldSearchable = [];
+
+    public function getFieldsSearchable(): array
+    {
+        return $this->fieldSearchable;
+    }
+
+    public function model(): string
+    {
+        return User::class;
+    }
 }
