d3d3d64c8505cb575d300234f2b28ee04bdd4504
Ali Ibrahim <aliibra395@gmail.com>
2022-04-28 19:47:10 +0200

    Project tasks status array

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -23,6 +23,29 @@ class Project extends Model
         return $this->hasMany(Task::class);
     }
 
+    public function countTasks($type, $limit = null)
+    {
+        $tasks = $this->hasMany(Task::class)->where('type', $type)->orderBy('created_at', 'desc');
+
+        if ($limit) {
+            $tasks->limit($limit);
+        }
+
+        return $tasks->get();
+    }
+
+    public function recentTasksStatus($type, $count = 5)
+    {
+        $tasks = $this->countTasks($type, $count);
+        $statusArr = [];
+
+        for ($i = 0; $i < $count; $i++) {
+            $statusArr[] = isset($tasks[$i]) ? $tasks[$i]->status : '⚪';
+        }
+
+        return $statusArr;
+    }
+
     public function User()
     {
         return $this->belongsTo(User::class);
